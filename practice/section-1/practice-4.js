e62b65eb33d64653896dd8155b387c41f3705dfb
xsu <xsu@thoughtworks.com>
2019-06-21 11:26:22 +0800

    finished section 1 practice 4

--- practice/section-1/practice-4.js
+++ practice/section-1/practice-4.js
@@ -1,5 +1,15 @@
 'use strict';
 
+/* eslint-disable no-unused-vars */
+
 function collectSameElements(collectionA, objectB) {
-  return '实现练习要求，并改写该行代码。';
+  const result = [];
+  for (let i = 0; i < collectionA.length; i++) {
+    if (objectB.value.indexOf(collectionA[i].key) !== -1) {
+      result.push(collectionA[i].key);
+    }
+  }
+  return result;
 }
+
+/* eslint-enable no-unused-vars */
