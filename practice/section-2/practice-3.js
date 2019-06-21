ef0607b7cd2dedc73f3c6b975c360c56097add0e
xsu <xsu@thoughtworks.com>
2019-06-21 11:27:36 +0800

    copied section 2 practice 2. refactor and finished section 2 practice 3.

--- practice/section-2/practice-3.js
+++ practice/section-2/practice-3.js
@@ -1,5 +1,31 @@
 'use strict';
 
+/* eslint-disable no-unused-vars */
+
 function countSameElements(collection) {
-  return '实现练习要求，并改写该行代码。';
+  const result = [];
+  for (let i = 0; i < collection.length; i++) {
+    if (collection[i].indexOf('-') !== -1) {
+      updateResult(result, collection[i].split('-')[0], parseInt(collection[i].split('-')[1]));
+    } else if (collection[i].indexOf(':') !== -1) {
+      updateResult(result, collection[i].split(':')[0], parseInt(collection[i].split(':')[1]));
+    } else if (collection[i].indexOf('[') !== -1) {
+      updateResult(result, collection[i].split('[')[0], parseInt(collection[i].split('[')[1].split(']')[0]));
+    } else {
+      updateResult(result, collection[i], 1);
+    }
+  }
+  return result;
+}
+
+/* eslint-enable no-unused-vars */
+
+function updateResult(result, key, count) {
+  for (let j = 0; j < result.length; j++) {
+    if (result[j]['name'] === key) {
+      result[j]['summary'] += count;
+      return;
+    }
+  }
+  result.push({name: key, summary: count});
 }
