ca76bbf6445991b489cc41ff4dce21bf32465b71
xsu <xsu@thoughtworks.com>
2019-06-21 11:28:48 +0800

    copie section 2. finished section 3 practice 3&4 once.

--- practice/section-3/practice-3.js
+++ practice/section-3/practice-3.js
@@ -1,5 +1,43 @@
 'use strict';
 
+/* eslint-disable no-unused-vars */
+
 function createUpdatedCollection(collectionA, objectB) {
-  return '实现练习要求，并改写该行代码。';
+  collectionA = countSameElements(collectionA);
+  for (let i = 0; i < objectB.value.length; i++) {
+    for (let j = 0; j < collectionA.length; j++) {
+      if (collectionA[j].key === objectB.value[i]) {
+        collectionA[j].count -= parseInt(collectionA[j].count / 3);
+      }
+    }
+  }
+  return collectionA;
+}
+
+/* eslint-enable no-unused-vars */
+
+function countSameElements(collection) {
+  const result = [];
+  for (let i = 0; i < collection.length; i++) {
+    if (collection[i].indexOf('-') !== -1) {
+      updateResult(result, collection[i].split('-')[0], parseInt(collection[i].split('-')[1]));
+    } else if (collection[i].indexOf(':') !== -1) {
+      updateResult(result, collection[i].split(':')[0], parseInt(collection[i].split(':')[1]));
+    } else if (collection[i].indexOf('[') !== -1) {
+      updateResult(result, collection[i].split('[')[0], parseInt(collection[i].split('[')[1].split(']')[0]));
+    } else {
+      updateResult(result, collection[i], 1);
+    }
+  }
+  return result;
+}
+
+function updateResult(result, key, count) {
+  for (let j = 0; j < result.length; j++) {
+    if (result[j]['key'] === key) {
+      result[j]['count'] += count;
+      return;
+    }
+  }
+  result.push({key: key, count: count});
 }
