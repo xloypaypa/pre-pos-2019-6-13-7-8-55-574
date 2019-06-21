d366ddc4b7c2f5422001ff8742983bc6662e4d8c
xsu <xsu@thoughtworks.com>
2019-06-21 11:27:00 +0800

    finished section 2 practice 2

--- practice/section-2/practice-2.js
+++ practice/section-2/practice-2.js
@@ -1,5 +1,31 @@
 'use strict';
 
+/* eslint-disable no-unused-vars */
+
 function countSameElements(collection) {
-  return '实现练习要求，并改写该行代码。';
+  const result = [];
+  for (let i = 0; i < collection.length; i++) {
+
+    if (collection[i].length !== 1) {
+      const key = collection[i].split('-')[0];
+      const count = parseInt(collection[i].split('-')[1]);
+      result.push({key: key, count: count});
+    } else {
+      let isNewElement = true;
+      for (let j = 0; j < result.length; j++) {
+        if (result[j]['key'] === collection[i]) {
+          isNewElement = false;
+          result[j]['count']++;
+          break;
+        }
+      }
+      if (isNewElement) {
+        result.push({key: collection[i], count: 1});
+      }
+    }
+  }
+  return result;
 }
+
+/* eslint-enable no-unused-vars */
+
