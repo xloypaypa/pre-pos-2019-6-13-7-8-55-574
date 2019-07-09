6a4c6b876d2826dd21d55c977ec76971a7212fe8
suxin <xloypaypa@gmail.com>
2019-07-09 21:02:08 +0800

    put result out.

--- practice/section-1/practice-1.js
+++ practice/section-1/practice-1.js
@@ -6,17 +6,21 @@ function collectSameElements(collectionA, collectionB) {
   const result = [];
   for (let i = 0; i < collectionA.length; i++) {
     const wanted = collectionA[i];
-    updateResultIfCollectionHaveWanted(result, collectionB, wanted);
+    const isInclude = includes(collectionB, wanted);
+    if (isInclude) {
+      result.push(wanted);
+    }
   }
   return result;
 }
 
-function updateResultIfCollectionHaveWanted(result, collection, wanted) {
+function includes(collection, wanted) {
   for (let i = 0; i < collection.length; i++) {
     if (collection[i] === wanted) {
-      result.push(collection[i]);
+      return true;
     }
   }
+  return false;
 }
 
 /* eslint-enable no-unused-vars */
