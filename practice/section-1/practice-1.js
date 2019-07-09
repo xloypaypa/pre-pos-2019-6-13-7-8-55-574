16cc67d962f50a4dafe70bcd55fabb91a4c6e9fb
suxin <xloypaypa@gmail.com>
2019-07-09 20:53:40 +0800

    extract a for loop out.

--- practice/section-1/practice-1.js
+++ practice/section-1/practice-1.js
@@ -5,10 +5,15 @@
 function collectSameElements(collectionA, collectionB) {
   const result = [];
   for (let i = 0; i < collectionA.length; i++) {
+    let wanted = collectionA[i];
+    let found = false;
     for (let j = 0; j < collectionB.length; j++) {
-      if (collectionB[j] === collectionA[i]) {
-        result.push(collectionA[i]);
+      if (collectionB[j] === wanted) {
+        found = true;
       }
+    }
+    if (found) {
+      result.push(collectionA[i]);
     }
   }
   return result;
