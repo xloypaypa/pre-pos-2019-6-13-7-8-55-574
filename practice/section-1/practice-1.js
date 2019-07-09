73c1992917094321130c312b928b2a552645437c
suxin <xloypaypa@gmail.com>
2019-07-09 20:55:22 +0800

    extract include out.

--- practice/section-1/practice-1.js
+++ practice/section-1/practice-1.js
@@ -6,17 +6,20 @@ function collectSameElements(collectionA, collectionB) {
   const result = [];
   for (let i = 0; i < collectionA.length; i++) {
     let wanted = collectionA[i];
-    let found = false;
-    for (let j = 0; j < collectionB.length; j++) {
-      if (collectionB[j] === wanted) {
-        found = true;
-      }
-    }
+    let found = includes(collectionB, wanted);
     if (found) {
       result.push(collectionA[i]);
     }
   }
   return result;
+}
+
+function includes(collection, wanted) {
+  for (let j = 0; j < collection.length; j++) {
+    if (collection[j] === wanted) {
+      return true;
+    }
+  }
 }
 
 /* eslint-enable no-unused-vars */
